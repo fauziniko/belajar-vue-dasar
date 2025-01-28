f23c5c26dd978826e41d656ee952ee1be42060ee
fauzi niko <fauziniko12@gmail.com>
2025-01-28 16:41:48 +0700

    [Misc] Update: src/components/TodoList.vue,src/todolist.js,todolist.html,

---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<script setup>
+const items = [
+  {
+    id: 1,
+    text: "Learn Vue 3",
+    done: true
+  },
+  {
+    id: 2,
+    text: "Learn Vue Router 2",
+    done: false
+  },
+  {
+    id: 3,
+    text: "Learn Vue Pinia",
+    done: false
+  },
+  {
+    id: 4,
+    text: "Learn Vue Pinia 2",
+    done: false
+  }
+];
+</script>
+
+<template>
+  <h1>Todo List</h1>
+  <template v-for="item in items">
+    <ul v-if="!item.done" :key="item.id">
+      <li v-for="(value, key) in item">
+        {{ key }}: {{ value }}
+      </li>
+    </ul>
+  </template>
+
+  <h1 v-for="i in 10">Hello {{ i }}</h1>
+</template>
+
+<style scoped>
+
+</style>
